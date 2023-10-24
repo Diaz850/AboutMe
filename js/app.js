cfd75adbc620376bc841cee39b006431cfb88361
Pedro Diaz Jr. <Diaz850@gmail.com>
2023-10-24 16:18:34 -0500

    working  with javascript

--- js/app.js
+++ js/app.js
@@ -1,42 +1,31 @@
 'use strict';
 
-// If conditional based confirm command
-let answer1 = confirm('Are you ready to rumble?');
-if (answer1 === true) {
-  console.log('LET\'S GET READY TO RUMBLE!!!');
-} else {
-  console.log('Oh. Well then.');
+const questions = [
+    ' Can you count to 10?',
+    ' Do you know the Alphabet? ',
+    ' Do you know right from wrong?',
+    ' Is there life beyond earth?',
+    ' Can we be friends?',
+]; 
+
+const answers = [
+    'yes','yes','no','yes','yes'
+]; 
+
+function askQuestions(){for 
+    (let i = 0; i < questions.length; i++) {
+    let userInput = prompt(questions[i]).toLowerCase(); // Normalize input to lowercase
+    if (userInput === answers[i] || userInput === "y" || userInput === "yes") {
+      alert("Correct!");
+    } else if (userInput === "n" || userInput === "no") {
+      alert("Incorrect!");
+    } else {
+      alert("Invalid input. Please answer with 'yes' or 'no'.");
+    }
+  }
 }
 
-// logical operator example
-let first = true;
-let second = false;
-let third = true;
+// Call the function to start asking questions
+askQuestions();
 
-// And = both conditions have to be true
-if (first && third) {
-  console.log('first and third were both true');
-} // only one of these have to be true
-else if (first || second) {
-  console.log('first or second was true!');
-} // only third can be true
-else if (third) {
-  console.log('first and second were false, but third was true.');
-} // none of the above conditions were true
-else {
-  console.log('Were any of my variables true?');
-}
-
-let color = prompt('What is your favorite color');
-
-// convert the variable of color to lower case and evaluate it
-switch (color.toLowerCase()) {
-case 'red':
-  console.log('Your favorite color was red!');
-  break;
-case 'blue':
-  console.log('Your favorite color was blue!');
-  break;
-default:
-  console.log('i don\'t know what your favorite color was ¯\\_(ツ)_/¯');
 }
