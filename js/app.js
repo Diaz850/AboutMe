e1c9d6a3f156d546e7939b3b9f5fde374fcff0b2
Pedro Diaz Jr. <Diaz850@gmail.com>
2023-10-24 22:20:05 -0500

    tried to adjust js but had a hard time

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 'use strict';
 
-const questions = [
+let userName = prompt("Hello! What is your name?");
+let greetingMessage = ("Welcome! "+ userName + ", Thanks for checking out my page!")
+
+alert(greetingMessage);
+
+// let finalMessage = prompt("Hope you enjoyed the page," + userName + "! Come back soon to check for updates!");
+
+// prompt(finalMessage);
+let questions = [
     ' Can you count to 10?',
     ' Do you know the Alphabet? ',
     ' Do you know right from wrong?',
@@ -8,12 +16,12 @@ const questions = [
     ' Can we be friends?',
 ]; 
 
-const answers = [
+let answers = [
     'yes','yes','no','yes','yes'
 ]; 
 
-function askQuestions(){for 
-    (let i = 0; i < questions.length; i++) {
+function askQuestions(){
+    for (let i = 0; i < questions.length; i++) {
     let userInput = prompt(questions[i]).toLowerCase(); // Normalize input to lowercase
     if (userInput === answers[i] || userInput === "y" || userInput === "yes") {
       alert("Correct!");
@@ -23,9 +31,11 @@ function askQuestions(){for
       alert("Invalid input. Please answer with 'yes' or 'no'.");
     }
   }
-}
 
 // Call the function to start asking questions
 askQuestions();
 
 }
+const finalMessage = prompt("Hope you enjoyed the page " + userName + "! Come back soon to check for updates!");
+
+alert(finalMessage);
